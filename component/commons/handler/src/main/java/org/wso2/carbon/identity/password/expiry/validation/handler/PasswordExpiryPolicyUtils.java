47a6abf3515437ab0d473ea342a2e7eec10a10cd
dinithih <dinithiliyanage.95@gmail.com>
2023-02-13 17:32:52 +0530

    Fix review suggestions

--- component/commons/handler/src/main/java/org/wso2/carbon/identity/password/expiry/validation/handler/PasswordExpiryPolicyUtils.java
+++ component/commons/handler/src/main/java/org/wso2/carbon/identity/password/expiry/validation/handler/PasswordExpiryPolicyUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2022, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.password.expiry.validation.handler;
 
 import org.apache.commons.logging.Log;
@@ -12,13 +28,19 @@ import org.wso2.carbon.idp.mgt.IdentityProviderManagementException;
 import org.wso2.carbon.idp.mgt.IdentityProviderManager;
 
 /**
- * This includes methods to read password expiry in days configuration.
+ * Read password expiry in days configuration.
  */
 public class PasswordExpiryPolicyUtils {
     private static final Log log = LogFactory.getLog(PasswordExpiryPolicyUtils.class);
 
     private PasswordExpiryPolicyUtils(){}
 
+    /**
+     * Get identity event property for password expiry in days.
+     * @param tenantDomain
+     * @param propertyName
+     * @return
+     */
     public static String getIdentityEventProperty(String tenantDomain, String propertyName) {
 
         // Retrieving properties set in identity event properties
@@ -31,6 +53,7 @@ public class PasswordExpiryPolicyUtils {
                 propertyValue = moduleConfiguration.getModuleProperties().getProperty(propertyName);
             }
         } catch (IdentityEventException e) {
+            //if identity property for password expiry in days can not be fetched, default value will be used.
             log.warn("An error occurred while retrieving module properties");
             if (log.isDebugEnabled()) {
                 log.debug("An error occurred while retrieving module properties because " + e.getMessage(), e);
@@ -39,13 +62,20 @@ public class PasswordExpiryPolicyUtils {
         return propertyValue;
     }
 
+    /**
+     * Get resident IDP property for password expiry in days configuration.
+     * @param tenantDomain
+     * @param propertyName
+     * @return
+     */
     public static String getResidentIdpProperty(String tenantDomain, String propertyName) {
 
         IdentityProvider residentIdP = null;
         try {
             residentIdP = IdentityProviderManager.getInstance().getResidentIdP(tenantDomain);
         } catch (IdentityProviderManagementException e) {
-
+        //if the residentIdp property could not be fetched, the identity event property password expiry in days
+            // configuration will be used.
         }
 
         if (residentIdP == null) {
